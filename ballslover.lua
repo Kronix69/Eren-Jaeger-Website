89ce14ff56387652f9e34f6c4723114fd598c98b
Emanuel Dicu <34407236+Kronix69@users.noreply.github.com>
2022-06-01 12:38:57 +0300

    Update ballslover.lua

--- ballslover.lua
+++ ballslover.lua
@@ -1,58 +1,62 @@
-local PlayerName = "Aesthetiful"
-local CharacterAppearance = "http://tadah.rocks/users/353/character"
-local ServerAddress = "147.185.221.229"
-local ServerPort = 60526
-
+--rbxsig%jf7mRT/qm1JSQz2PuEOtZvQufquXzX7Md+bZ00xHbqtjymCidkjx0bWTqn3DJNEImgMZ9EvWkBny3YQ/rfpxOfA9DOZeArur/EV+slgd1aDeD8OpXerAvM8J3/u+WYg4b1S83+LEoJPoHiSg+IgJZcOq5m4x6eEs1SFclAIntYU=%
 local Players = game:GetService("Players")
-Players:SetChatStyle(Enum.ChatStyle.ClassicAndBubble)
-
-local Player = Players:CreateLocalPlayer(353)
-Player.Name = PlayerName
-Player.CharacterAppearance = CharacterAppearance
-
-local PlayerToken = Instance.new("StringValue", Player)
-PlayerToken.Name = "token"
-PlayerToken.Value = "1LSykZLkYGeBZq4aZ3Hq"
-
-local Visit = game:GetService("Visit")
-Visit:SetUploadUrl("")
-
-game:SetMessage("Connecting to server...")
 local NetworkClient = game:GetService("NetworkClient")
-NetworkClient:Connect(ServerAddress, ServerPort)
+local ScriptContext = game:GetService("ScriptContext")
+
+-- 1320 for mid 2014 corescripts and 2006 for late 2014
+ScriptContext:AddStarterScript(2006)
+
+NetworkClient:PlayerConnect(353, "147.185.221.229", 60526)
+
+local LocalPlayer = Players.LocalPlayer
+LocalPlayer.Name = "Aesthetiful"
+LocalPlayer.CharacterAppearance = "http://tadah.rocks/users/353/character?tick=" .. tick()
+
+local PlayerToken = Instance.new("StringValue", LocalPlayer)
+PlayerToken.Name = "token"
+PlayerToken.Value = "LazmlernNzYP206nqHh3"
+
+pcall(function() LocalPlayer:SetMembershipType(0) end)
+pcall(function() LocalPlayer:SetSuperSafeChat(false) end)
+pcall(function() Players:SetChatStyle(2) end)
+
+pcall(function() game:SetCreatorId(1555, Enum.CreatorType.User) end)
+pcall(function() game:SetPlaceId(732) end)
+
+game:SetMessage("Connecting to server")
 
 NetworkClient.ConnectionAccepted:connect(function(Peer, NetworkReplicator)
-	NetworkReplicator.Disconnection:connect(function(Peer, LostConnection)
-		if LostConnection then
-			game:SetMessage("You have lost connection to the game")
-		else
-			game:SetMessage("This game has shut down")
-		end
-	end)
+    NetworkReplicator.Disconnection:connect(function(Peer, LostConnection)
+        if LostConnection then
+            game:SetMessage("You have lost connection to the game")
+        else
+            game:SetMessage("This game has shut down")
+        end
+    end)
 
-	game:SetMessageBrickCount()
+    game:SetMessageBrickCount()
 
-	local MarkerReceived = false
+    local MarkerReceived = false
 
-	local NetworkMarker = NetworkReplicator:SendMarker()
-	NetworkMarker.Received:connect(function()
-		MarkerReceived = true
+    local NetworkMarker = NetworkReplicator:SendMarker()
+    NetworkMarker.Received:connect(function()
+        MarkerReceived = true
 
-		game:SetMessage("Requesting character")
-		NetworkReplicator:RequestCharacter()
+        game:SetMessage("Requesting character")
+        NetworkReplicator:RequestCharacter()
 
-		game:SetMessage("Waiting for character")
-		Player.CharacterAdded:connect(function()
-			game:ClearMessage()
-		end)
-	end)
+        game:SetMessage("Waiting for character")
+        LocalPlayer.CharacterAdded:connect(function()
+            game:ClearMessage()
+        end)
+    end)
 
-	while not MarkerReceived do
-		workspace:ZoomToExtents()
-		wait(0.5)
-	end
+    while not MarkerReceived do
+        workspace:ZoomToExtents()
+        wait(0.5)
+    end
 end)
 
 NetworkClient.ConnectionFailed:connect(function(Peer, ErrorCode, ErrorMessage)
-	game:SetMessage(string.format("Failed to connect to the Game. (ID=%d)", ErrorCode))
+    game:SetMessage(string.format("Failed to connect to the Game. (ID=%d)", ErrorCode))
 end)
